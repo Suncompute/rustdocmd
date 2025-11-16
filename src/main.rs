aa6f7fce65a90945064a7f8a0869a5e941ee8cee
Bruno <222108709+Suncompute@users.noreply.github.com>
2025-11-16 10:26:26 +0000

    Update documentation in introducing.md and main.rs
    - Revised the introduction in introducing.md to enhance clarity and added a note about the tool's prototype status.
    - Updated the corresponding documentation comments in main.rs to reflect the changes made in introducing.md, ensuring consistency across the documentation.
    - Added a "Buy Me a Coffee" link to support the ongoing development of the tool.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 
 /// <introducing.md(1)> "main.rs"
 /// <readme>
-/// # rustdocmd
+/// rustdocmd
 /// 
 /// Mit diesem Tool kannst du direkt im Rust-Code mit rustdoc-Kommentaren (`///` oder `//!`) umfangreiche, versionierte Dokumentation verfassen.
+///
 /// Speziell markierte Bereiche werden automatisch extrahiert und als eigenständige Markdown-Dateien ausgegeben.
+///
 /// So entsteht aus deinem Code und den Kommentaren eine vollständige, stets aktuelle Dokumentation, die sich nahtlos und automatisiert in Systeme wie mdBook integrieren und veröffentlichen lässt – sodass deine Projektdokumentation immer automatisch mit dem aktuellen Stand deines Codes übereinstimmt.
+///
+/// Das Tool befindet sich noch im Prototyp-Stadium und wird kontinuierlich weiterentwickelt.
+///
+/// 👉 [Buy Me a Coffee](https://www.buymeacoffee.com/suncompute)
+
+
 /// </readme>
 /// </introducing.md>
 /// <install.md(2)> "main.rs"
